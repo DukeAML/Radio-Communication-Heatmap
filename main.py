84a25a1e5f0961a81294bb7ef1cd93ec8bb0016d
Tyler Feldman <tylerfeldman321@gmail.com>
2020-12-10 13:50:05 -0800

    added basic flask code

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import numpy as np
 from flask import Flask
+from elevation_data import *
+from heatmap_generation import compute_heatmap
 
 
 '''
 What we need to do:
 
-import dted data
+import dted data or other elevation data
 generate heatmap - needs to be fleshed out further
 provide results to the front end
 
 '''
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/test')
+def test():
+    stats, carpet = get_airmap_data(47.6062, 122.3321)
+    heatmap = compute_heatmap(carpet)
+    return 'done'
