f512cb64d59e638f81f90165124790aac4ca5e1e
Tyler Feldman <tylerfeldman321@gmail.com>
2020-12-16 16:25:46 -0800

    Added bilinear interpolation to rotate_filter

--- heatmap_generation.py
+++ heatmap_generation.py
@@ -24,13 +24,15 @@ def compute_heatmap(elevation_grid):
 def rotate_filter(filter, degrees):
     
     img = Image.fromarray(filter)
-    rotated_img = img.rotate(degrees)
+    rotated_img = img.rotate(degrees, resample=Image.BILINEAR)
     rotated_filter = np.asarray(rotated_img)
-    
+
     return rotated_filter
 
 def calculate_bearing_angle(lat1, lng1, lat2, lng2):
     # https://www.igismap.com/formula-to-find-bearing-or-heading-angle-between-two-points-latitude-longitude/
+    # https://stackoverflow.com/questions/3932502/calculate-angle-between-two-latitude-longitude-points
+    
     lat1, lng1, lat2, lng2 = math.radians(lat1), math.radians(lng1), math.radians(lat2), math.radians(lng2)
     delta_lng = lng2-lng1
 
@@ -45,8 +47,11 @@ def calculate_bearing_angle(lat1, lng1, lat2, lng2):
 
 if __name__ == "__main__":
     
+    # Test to make sure calculate_bearing_angle works
     print(calculate_bearing_angle(39.099912, -94.581213, 38.627089, -90.200203)) # Should return 96.51
+    print(calculate_bearing_angle(8.46696, -17.03663, 65.35996, -17.03663)) # Should return 0 degrees
     
+    # Test to make sure compute_heatmap works
     stats, carpet = get_airmap_data(47.6062, 122.3321)
     print(carpet)
     heatmap, best_loc = compute_heatmap(carpet)
@@ -59,7 +64,7 @@ if __name__ == "__main__":
     plt.plot(best_loc[1], best_loc[0], 'ro')
     plt.imshow(heatmap)
 
-    
+    # Test to make sure rotate_filter works
     sample_filter = np.array([[0, -2, -2, -2, 0],
                               [1, .5, -2, .5, 1],
                               [1, .5, -2, .5, 1],
@@ -68,11 +73,11 @@ if __name__ == "__main__":
 
     plt.figure(3)
     plt.imshow(sample_filter)
-    plt.title("sample_filter")
+    plt.title("Sample Filter")
 
     plt.figure(4)
     plt.imshow(rotate_filter(sample_filter, 45))
-    plt.title("rotated_filter")
+    plt.title("Rotated Filter")
     plt.show()
 
 
